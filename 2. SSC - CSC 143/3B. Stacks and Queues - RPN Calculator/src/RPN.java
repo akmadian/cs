b95b06d8f7c4628b9b4209fc4db83b7b9eff8e96
Ari Madian <akmadian@gmail.com>
2019-03-03 17:44:25 -0800

    Update project 3B

--- 2. SSC - CSC 143/3B. Stacks and Queues - RPN Calculator/src/RPN.java
+++ 2. SSC - CSC 143/3B. Stacks and Queues - RPN Calculator/src/RPN.java
@@ -1,16 +1,14 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.*;
 
 /**
- * TODO:
- * 
- * @author 
+ * @author Ari Madian
  *
+ *  Calculates the value of a mathematical expression in RPN format.
  */
 public class RPN {
 
     /**
-     * TODO: 
-     * 
      * @param args
      */
     public static void main(String[] args) {
@@ -45,50 +43,75 @@ public class RPN {
     }
     
     /**
-     * TODO:     
+     * Evaluates a Reverse Polish Notation (RPN) expression.
+     *
+     * Ex: 2 2 + => 4.0,
+     *     5 2 2 * - 1 2 + / => 0.333
      *  
-     * @param input - TODO
-     * @return double - TODO
+     * @param input - A mathematical expression in RPN format.
+     * @return double - The value of the given RPN expression.
      */
     public static double evaluateRPN(String input) {
-        // Create queue, transfer input into it "2 3 +" -> [2 3 +]
+        LinkedList<String> expression = new LinkedList<String>(
+                Arrays.asList(input.split(" "))
+        );
+        int expressionSize = expression.size(); // Save original expression size
+        System.out.println(expression);
 
-        // Print input
+        Stack<String> stack = new Stack<>();
+        for (int op = 0; op < expressionSize; op++) {
+            if (expression.size() <= 0) break; // If nothing left in expression to process
+            if (isOperator(expression.get(op))) { // If looking at operator
+                double operand2 = Double.parseDouble(stack.pop());
+                double operand1 = Double.parseDouble(stack.pop());
+                stack.push(evaluateBinaryOperator(operand1, expression.get(op), operand2)); // Calulate, push onto stack
 
-        // Create new empty stack
+                expression.remove(op); // Remove operator from expression
+                op--;
 
-        // Pop off each item in the queue and evaluate it
-                // if operator such as '*' - pop two operands, evaluate, push result: Queue [+] Stack [2 3] => [][5]
+                System.out.print(expression); System.out.println(stack);
+            } else { // If NOT an operator
+                stack.push(expression.get(op)); // Push operand onto stack
 
-                // else operands such as "5" just need to be pushed [3 +][2]=>[+][2 3]
+                expression.remove(op); // Remove operand from expression
+                op--;
+            }
+        }
 
-        // return last item in stack [][5]
+        return Double.parseDouble(stack.pop());
     }
 
     /**
-     * TODO:     
-     *
-     * @param input - TODO: 
-     * @return boolean - TODO: 
+     * Helper method to discern whether or not a given string value is a mathematical operator.
+     * Ex: "*" => true, "a" => false, "+" => true
+     * @param input - The string value to test
+     * @return boolean - Whether or not the given value is a mathematical operator.
      */
     private static boolean isOperator(String input) {
         // Check if the String is one of the + - * / characters 
-        // Either compare against each character individually or use String.contains 
+        // Either compare against each character individually or use String.contains
+        return (input.equals("*") || input.equals("/") || input.equals("+") || input.equals("-"));
     }
 
     /**
-     * TODO:     
+     * Helper method to compute the value of a two-operand expression.
      * 
      * evaluateBinaryOperator(2, "+", 3)=> "5"
      * 
-     * @param op1 - TODO: 
-     * @param operator - TODO: 
-     * @param op2 - TODO: 
-     * @return TODO: 
+     * @param op1 - The left operand of the expression
+     * @param operator - The operator of the expression
+     * @param op2 - The right operand of the expression
+     * @return The expression's value.
      */
     private static String evaluateBinaryOperator(Double op1, String operator, Double op2) {
-        // Individual if/else or case switch to find operator + - * /
-            // Return op1 <operator>
+        switch (operator) {
+            case "*": return String.valueOf(op1 * op2);
+            case "+": return String.valueOf(op1 + op2);
+            case "-": return String.valueOf(op1 - op2);
+            case "/": return String.valueOf(op1 / op2);
+        }
+
+        return null;
     }
 
 }
