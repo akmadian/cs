80c5125383e4f56e2808a42192557ced8d4e70f0
Ari Madian <akmadian@gmail.com>
2019-03-17 15:53:11 -0700

    Cleanup, add docs and comments

--- 2. SSC - CSC 143/4. Tiny123/src/Sheet.java
+++ 2. SSC - CSC 143/4. Tiny123/src/Sheet.java
@@ -3,58 +3,83 @@ import java.util.LinkedList;
 public class Sheet {
     private LinkedList<SheetRow> rows;
 
+    /**
+     * Constructs a sheet
+     */
     public Sheet() {
         rows = new LinkedList<>();
 
         for (int i = 1; i <= 9; i++) {
-            rows.add(new SheetRow(i));
+            rows.add(new SheetRow(i)); // Populate rows
         }
     }
 
+    /**
+     * Inserts a new row into the sheet
+     * @param row The index at which to insert the row
+     */
     public void insertRow(int row) {
         rows.add(row, new SheetRow(row));
     }
 
+    /**
+     * Deletes a row from the sheet
+     * @param row The index of the row to delete
+     */
     public void deleteRow(int row) {
         rows.remove(row);
     }
 
+    /**
+     * Inserts a column at the given index
+     * @param col The index at which to insert the column
+     */
     public void insertCol(int col) {
         for (int i = 0; i < rows.size(); i++) {
             rows.get(i).addCell(col, "R" + String.valueOf(i) + "C" + String.valueOf(col));
         }
     }
 
+    /**
+     * Deletes a column from the sheet
+     * @param col The index of the column to delete
+     */
     public void deleteCol(int col) {
         for (int i = 0; i < rows.size(); i++) {
             rows.get(i).removeCell(col);
         }
     }
 
+    /**
+     * Sets the value of a cell
+     * @param row The cell's row
+     * @param col The cell's column
+     * @param s The value to set
+     */
     public void setValue(int row, int col, String s) {
         rows.get(row).setValue(col, s);
     }
 
+    /**
+     * Converts the sheet to a string
+     * @return The cell with all values and sums
+     */
     public String toString() {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // Sheet sb
 
-        for (SheetRow row : rows) {
-            try {
-                sb.append(row.toString() + "\n");
-            } catch (IndexOutOfBoundsException e) { }
+        for (SheetRow row : rows) { // Append each row
+            sb.append(row.toString() + "\n");
         }
 
-        StringBuilder sumRow = new StringBuilder();
+        StringBuilder sumRow = new StringBuilder(); // Sum row sb
         int numCols = -1;
         try {
-            numCols = rows.get(0).numCols();
-        } catch (IndexOutOfBoundsException e) { }
+            numCols = rows.get(0).size();          // Get the number of columns in the sheet
+        } catch (IndexOutOfBoundsException e) { }  // If there are no rows, catch exception, do nothing
 
-        if (numCols > 0) {
-            for (int i = 0; i < rows.get(0).numCols(); i++) {
-                try {
-                    sumRow.append(String.format("%-8s", "Sum=" + getColumnSum(i)));
-                } catch (IndexOutOfBoundsException e) { }
+        if (numCols > 0) { // If there are columns in the sheet
+            for (int i = 0; i < rows.get(0).size(); i++) {
+                sumRow.append(String.format("%-8s", "Sum=" + getColumnSum(i))); // Add each column's sum
             }
             sumRow.append(String.format("%-8s", "Sum=0"));
         }
@@ -63,18 +88,27 @@ public class Sheet {
         return sb.toString();
     }
 
+    /**
+     * Computes the sum of a column
+     * @param col The index of the column
+     * @return The sum of all cells in a given column
+     */
     int getColumnSum(int col) {
         int colSum = 0;
 
-        for (SheetRow row : rows) {
+        for (SheetRow row : rows) { // For row in sheet
             try {
-                colSum += row.getValue(col);
-            } catch (NumberFormatException e) { }
+                colSum += row.getValue(col); // Try to add the cell's value
+            } catch (NumberFormatException e) { } // If NaN, catch, do nothing
         }
 
         return colSum;
     }
 
+    /**
+     * Computes the sum of all cells in a sheet
+     * @return The sum of all cells in the sheet
+     */
     int getSheetSum() {
         int sheetSum = 0;
 
--- 2. SSC - CSC 143/4. Tiny123/src/SheetRow.java
+++ 2. SSC - CSC 143/4. Tiny123/src/SheetRow.java
@@ -4,51 +4,87 @@ public class SheetRow {
 
     private LinkedList<String> cells = new LinkedList<>();
 
+
+    /**
+     * Constructs a SheetRow instance
+     * @param row The row the SheetRow will be constructed at
+     */
     public SheetRow(int row) {
-        String cellValBase = "R" + String.valueOf(row) + "C";
+        String cellValBase = "R" + String.valueOf(row) + "C"; // Create base cell value
         for (int i = 1; i <= 9; i++) {
-            cells.add(cellValBase + String.valueOf(i));
+            cells.add(cellValBase + String.valueOf(i)); // Add cells
         }
     }
 
+    /**
+     * Adds a cell to the SheetRow instance's
+     * @param index Which index to add the cell at
+     * @param s The value to add the cell to
+     */
     public void addCell(int index, String s) {
         cells.add(index, s);
     }
 
+    /**
+     * Removes the cell at the given index
+     * @param index The index of the cell to remove
+     */
     public void removeCell(int index) {
         cells.remove(index);
     }
 
+    /**
+     * Sets the value of a given cell to a given value
+     * @param cell The index of the cell to set
+     * @param s The value to set
+     */
     public void setValue(int cell, String s) {
         cells.set(cell, s);
     }
 
+    /**
+     * Converts the row to a string
+     * @return A string with all cells and the row's sum
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (String val : cells) {
-            sb.append(String.format("%-8s", val));
+            sb.append(String.format("%-8s", val)); // Append a cell value
         }
-        sb.append("Sum = " + getSum());
+        sb.append("Sum = " + getSum()); // Append the row's sum
         return sb.toString();
     }
 
+    /**
+     * Gets the value of a given cell
+     * @param cell The index of the cell to get
+     * @return The cell's value
+     */
     int getValue(int cell) {
         return Integer.parseInt(cells.get(cell));
     }
 
+    /**
+     * Gets the sum of all cells in the row
+     * @return The sum of all cells in the row
+     */
     int getSum() {
         int sum = 0;
 
-        for (String val : cells) {
+        for (String val : cells) { // For each cell
             try {
-                sum += Integer.parseInt(val);
-            } catch (NumberFormatException e) {}
+                sum += Integer.parseInt(val); // Try to add the value to the sum
+            } catch (NumberFormatException e) {} // If NaN, catch exception and do nothing
         }
 
         return sum;
     }
 
-    int numCols() {
+    /**
+     * Gets the size of the row
+     * @return The number of cells in the row
+     */
+    int size() {
         return cells.size();
     }
 }
