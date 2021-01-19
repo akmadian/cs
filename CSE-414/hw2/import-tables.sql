6dea887b42f52287d0c936f337f289d96058c7ee
Ari Madian <akmadian@gmail.com>
2021-01-18 18:25:28 -0800

    Add info-340 problem set 2 and some work on cse-414 hw2

---
+++ CSE-414/hw2/import-tables.sql
@@ -0,0 +1,6 @@
+PRAGMA foreign_keys=ON;
+.mode csv
+.import "flights-dataset/flight-dataset/carriers.csv" CARRIERS
+.import "flights-dataset/flight-dataset/months.csv" MONTHS
+.import "flights-dataset/flight-dataset/weekdays.csv" WEEKDAYS
+.import "flights-dataset/flight-dataset/flights-small.csv" FLIGHTS
