3830fefaf6bced26c00ae66962cacfc47c83f5a0
Ari Madian <akmadian@gmail.com>
2021-02-01 21:07:04 -0800

    CSE-414 cleanup

--- CSE-414/hw3/import-tables.sql
+++
@@ -1,6 +0,0 @@
-PRAGMA foreign_keys=ON;
-.mode csv
-.import "flights-dataset/carriers.csv" CARRIERS
-.import "flights-dataset/months.csv" MONTHS
-.import "flights-dataset/weekdays.csv" WEEKDAYS
-.import "flights-dataset/flights-small.csv" FLIGHTS
